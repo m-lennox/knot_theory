d9a305a6a33a63df437fec6bb7b80eac2826bfa6
Lennox <lennox_8@icloud.com>
2025-10-23 19:08:40 +0800

    switch to gauss codes

--- KnotTheory/Basic.lean
+++ KnotTheory/Basic.lean
@@ -1,33 +1,55 @@
-structure OrientedDiagramNode where
+import Mathlib.Data.Finset.Basic
+import Mathlib.Algebra.Group.Int.Even
+
+abbrev Node := {n : Nat // n ≠ 0}
+
+structure GaussSegment where
   private mk ::
-  o1 : Nat
-  o2 : Nat
-  u1 : Nat
-  u2 : Nat
-  distinct : o1 ≠ o2 ∧ u1 ≠ u2
+  n : Node
+  ou : Bool
 
-def mkOrientedDiagramNode (o1 o2 u1 u2 : Nat) : Option OrientedDiagramNode :=
-  if h : o1 ≠ o2 ∧ u1 ≠ u2 then
-    some { o1, o2, u1, u2, distinct := h }
-  else
-    none
+instance : Repr GaussSegment where
+  reprPrec
+    | ⟨n, ou⟩, _ => s!"{if ou then "O" else "U"}{n}"
 
-def OrientedDiagramNode.segments (self : OrientedDiagramNode) : List Nat :=
-  [self.o1, self.o2, self.u1, self.u2]
+namespace GaussSegment
 
-structure OrientedPreDiagram where
-  nodes : Array OrientedDiagramNode
+def mkSegmentFromInt (n : Int) (h : n ≠ 0 := by decide) : GaussSegment :=
+  let h1 : n.natAbs ≠ 0 := by simp [Int.natAbs_eq_zero]; tauto
+  {n := ⟨n.natAbs, h1⟩, ou := n > 0}
 
-def OrientedPreDiagram.crossing_num (self : OrientedPreDiagram) : Nat :=
-  self.nodes.size
+def toInt (self : GaussSegment) : Int :=
+  (self.ou.toInt * 2 - 1) * self.n
 
-def num_in (nodes : Array OrientedDiagramNode) (segment : Nat) : Nat :=
-  (nodes.map (fun node => ((node.o1 == segment).toNat + (node.u1 == segment).toNat))).sum
+theorem segment_to_int_ne_zero (self : GaussSegment) : self.toInt ≠ 0 := by
+  simp [toInt]
+  constructor
+  · rw [Int.mul_comm, ← Int.zero_dvd]
+    have h : (2: Int) ∣ 0 := by simp
+    intro x
+    have i := Int.dvd_trans h x
+    have y := Int.two_not_dvd_two_mul_add_one (self.ou.toInt - 1)
+    have z : (2: Int) - 1 = 1 := by simp
+    rw [Int.mul_sub, sub_add, mul_one, z] at y
+    tauto
+  have x := self.n.prop
+  tauto
 
-def num_out (nodes : Array OrientedDiagramNode) (segment : Nat) : Nat :=
-  (nodes.map (fun node => ((node.o2 == segment).toNat + (node.u2 == segment).toNat))).sum
+end GaussSegment
 
-structure OrientedDiagram extends OrientedPreDiagram where
-  segment_node_correspondence : ∀ s : Nat, (num_in nodes s = num_out nodes s) ∧ (num_in nodes s ∈ [0, 1])
+structure GaussComponent where
+  segments : Array GaussSegment
 
-#eval (OrientedDiagramNode.mk 1 2 3 4 (by simp)).segments
+namespace GaussComponent
+
+def incidence (self : GaussComponent) (node : Node) (ou : Bool) : Nat :=
+  (self.segments.map (fun s => (s.n == node).toNat * (s.ou == ou).toNat)).sum
+
+end GaussComponent
+
+structure VirtualLinkDiagram where
+  nodes : Finset Node
+  components : Array GaussComponent
+  compatible : ∀ c ∈ components, ∀ s ∈ c.segments, s.n ∈ nodes
+  complete_over : ∀ n ∈ nodes, (components.map (·.incidence n true)).sum = 1
+  complete_under : ∀ n ∈ nodes, (components.map (·.incidence n false)).sum = 1
